383f05e2fb958315edabbd51105dd60f3657bddb
rafaeldoria <ieieldoria@gmail.com>
2025-02-06 20:11:42 -0300

    init schedule

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,169 @@
-import Image from "next/image";
+"use client"
+
+import { useState } from "react"
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [page, setPage] = useState(1)
+  const [isVisible, setIsVisible] = useState(false)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  function alterPage()
+  {
+    setIsVisible((prev) => !prev);
+    if (page == 1) {
+      setPage(2)
+    } else {
+      setPage(1)
+    }
+    
+  }
+
+  return (
+    <main>
+      <div className="w-full flex items-center px-2 py-4 bg-slate-400 h-2 shadow-sm">
+        <div className="w-full max-w-7xl mx-auto flex items-center text-white">
+          SCHEDULE
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+
+      <div className="w-full max-w-7xl mx-auto px-2 py-4 mt-4 bg-red-300">
+        <div className="flex items-center justify-center">
+          <section className={`flex-1 bg-blue-400 p-3 flex-col gap-4 ${
+              isVisible ? "hidden" : "flex"
+            } md:flex  `}>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-3">
+              <p className="text-center">Monday </p>
+              <p className="text-center">03/04/2025 </p>
+            </div>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-5">
+              <p>08:00</p>
+              <button className="text-sm text-green-700">OPEN</button>
+            </div>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-5">
+              <p>08:30</p>
+              <button className="text-sm text-red-700">CLOSED</button>
+            </div>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-5">
+              <p>09:00</p>
+              <button className="text-sm text-blue-700">SCHEDULED</button>
+            </div>
+          </section>
+
+          <section className={`flex-1 bg-blue-400 p-3 flex-col gap-4 ${
+            isVisible ? "hidden" : "flex"
+          } md:flex  `}>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-3">
+              <p className="text-center">Tuesday </p>
+              <p className="text-center">04/04/2025 </p>
+            </div>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-5">
+              <p>08:00</p>
+              <button className="text-sm text-green-700">OPEN</button>
+            </div>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-5">
+              <p>08:30</p>
+              <button className="text-sm text-green-700">OPEN</button>
+            </div>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-5">
+              <p>09:00</p>
+              <button className="text-sm text-blue-700">SCHEDULED</button>
+            </div>
+          </section>
+
+          <section className={`flex-1 bg-blue-400 p-3 flex-col gap-4 ${
+            isVisible ? "hidden" : "flex"
+          } md:flex  `}>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-3">
+              <p className="item-center">Wednesday </p>
+              <p className="item-center">05/04/2025 </p>
+            </div>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-5">
+              <p>08:00</p>
+              <button className="text-sm text-green-700">OPEN</button>
+            </div>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-5">
+              <p>08:30</p>
+              <button className="text-sm text-green-700">OPEN</button>
+            </div>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-5">
+              <p>09:00</p>
+              <button className="text-sm text-green-700">OPEN</button>
+            </div>
+          </section>
+
+          <section className={`flex-1 bg-blue-400 p-3 flex-col gap-4 ${
+            isVisible ? "flex" : "hidden"
+          } md:flex  `}>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-3">
+              <p className="text-center">Thursday </p>
+              <p className="text-center">06/04/2025 </p>
+            </div>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-5">
+              <p>08:00</p>
+              <button className="text-sm text-green-700">OPEN</button>
+            </div>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-5">
+              <p>08:30</p>
+              <button className="text-sm text-green-700">OPEN</button>
+            </div>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-5">
+              <p>09:00</p>
+              <button className="text-sm text-green-700">OPEN</button>
+            </div>
+          </section>
+
+          <section className={`flex-1 bg-blue-400 p-3 flex-col gap-4 ${
+            isVisible ? "flex" : "hidden"
+          } md:flex  `}>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-3">
+            <p className="text-center">Friday </p>
+            <p className="text-center">07/04/2025 </p>
+            </div>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-5">
+              <p>08:00</p>
+              <button className="text-sm text-red-700">CLOSED</button>
+            </div>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-5">
+              <p>08:30</p>
+              <button className="text-sm text-red-700">CLOSED</button>
+            </div>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-5">
+              <p>09:00</p>
+              <button className="text-sm text-red-700">CLOSED</button>
+            </div>
+          </section>
+
+          <section className={`flex-1 bg-blue-400 p-3 flex-col gap-4 ${
+            isVisible ? "flex" : "hidden"
+          } md:flex  `}>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-3">
+              <p className="text-center">Saturday </p>
+              <p className="text-center">08/04/2025 </p>
+            </div>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-5">
+              <p>08:00</p>
+              <button className="text-sm text-red-700">CLOSED</button>
+            </div>
+            <div className="flex flex-col items-center justify-center border rounded-md border-blue-300 bg-white p-5">
+              <p>08:30</p>
+              <button className="text-sm text-red-700">CLOSED</button>
+            </div>
+            <div className="flex flex-col  items-center justify-center border rounded-md border-blue-300 bg-white p-5">
+              <p>09:00</p>
+              <button className="text-sm text-red-700">CLOSED</button>
+            </div>
+          </section> 
+        </div>
+
+        <div className="mt-4 bg-slate-100 flex justify-end md:hidden">
+        {page == 1 ? (
+           <button className="text-slate-800 font-bold mr-2" onClick={alterPage}>NEXT</button>
+        ) : (
+          <button className="text-slate-800 font-bold mr-2" onClick={alterPage}>BACK</button>
+        )}
+        </div>
+        
+      </div>
+      
+    </main>
   );
 }
